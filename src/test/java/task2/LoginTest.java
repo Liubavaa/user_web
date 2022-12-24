a7e9f948ccb07102ab31827a8fd616f4126699a0
Liubavaa <liubomyr.oleksiuk@ucu.edu.ua>
2022-12-25 00:09:05 +0200

    try to fix dependencies (last)

--- src/test/java/task2/LoginTest.java
+++ src/test/java/task2/LoginTest.java
@@ -1,9 +1,14 @@
 package task2;
 
+import org.testng.annotations.Test;
+
 import java.util.Objects;
 
-public class TestLogin {
-    static {
+import static org.testng.Assert.*;
+
+public class LoginTest {
+    @Test
+    public void testLogin() {
         Client client1 = Login.login(LoginMethod.Facebook);
         Client client2 = Login.login(LoginMethod.Twitter);
         assert (Objects.equals(client1.getCountry(), client2.getCountry()));
